0f5005188fce164978c12afd150a162f9c866f19
Dmitriy Shweew <shweew@it-advisor.ru>
2021-03-11 06:58:33 +0400

    Update zabbix-agent-install.sh

--- zabbix-agent-install.sh
+++ zabbix-agent-install.sh
@@ -3,32 +3,33 @@
 #  Copyright (C) 2021 Dmitriy Shweew
 #  https://it-advisor.ru <shweew@it-advisor.ru>  
 
+CONF=/etc/zabbix/zabbix_agentd.conf
+
 if [ -n "$1" ]
 then
 
 #Installing Package
 rpm -ihv https://repo.zabbix.com/zabbix/5.2/rhel/7/x86_64/zabbix-agent-5.2.5-1.el7.x86_64.rpm
-cd /etc/zabbix
 
 #Generate a key
-openssl rand -hex 32 > zabbix_agent.psk
-chown zabbix:zabbix zabbix_agent.psk
-chmod 400 zabbix_agent.psk
+openssl rand -hex 32 > /etc/zabbix/zabbix_agent.psk
+chown zabbix:zabbix /etc/zabbix/zabbix_agent.psk
+chmod 400 /etc/zabbix/zabbix_agent.psk
 
 #Set up a configuration file
-cp zabbix_agentd.conf zabbix_agentd.conf.old
-echo "PidFile=/var/run/zabbix/zabbix_agentd.pid" > zabbix_agentd.conf
-echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> zabbix_agentd.conf
-echo "LogFileSize=0" >> zabbix_agentd.conf
-echo "Server=$1" >> zabbix_agentd.conf
-echo "ServerActive=$1" >> zabbix_agentd.conf
-echo "Hostname=$(hostname)" >> zabbix_agentd.conf
-echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> zabbix_agentd.conf
-echo "TLSConnect=psk" >> zabbix_agentd.conf
-echo "TLSAccept=psk" >> zabbix_agentd.conf
-echo "TLSPSKIdentity=PSK-$(hostname -s)" >> zabbix_agentd.conf
-echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> zabbix_agentd.conf
-echo >> zabbix_agentd.conf
+cp $CONF $CONF.old
+echo "PidFile=/var/run/zabbix/zabbix_agentd.pid" > $CONF
+echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> $CONF
+echo "LogFileSize=0" >> $CONF
+echo "Server=$1" >> $CONF
+echo "ServerActive=$1" >> $CONF
+echo "Hostname=$(hostname)" >> $CONF
+echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> $CONF
+echo "TLSConnect=psk" >> $CONF
+echo "TLSAccept=psk" >> $CONF
+echo "TLSPSKIdentity=PSK-$(hostname -s)" >> $CONF
+echo "TLSPSKFile=/etc/zabbix/zabbix_agent.psk" >> $CONF
+echo >> $CONF
 
 #Create a firewall rule and enable zabbix-agent startup
 firewall-cmd --permanent --new-service=zabbix
