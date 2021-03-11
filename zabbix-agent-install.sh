893f0e47ff11177b4595268bab63bedb1b393d7b
Dmitriy Shweew <shweew@it-advisor.ru>
2021-03-11 07:02:25 +0400

    Update zabbix-agent-install.sh

--- zabbix-agent-install.sh
+++ zabbix-agent-install.sh
@@ -4,6 +4,7 @@
 #  https://it-advisor.ru <shweew@it-advisor.ru>  
 
 CONF=/etc/zabbix/zabbix_agentd.conf
+KEY=/etc/zabbix/zabbix_agentd.psk
 
 if [ -n "$1" ]
 then
@@ -12,9 +13,9 @@ then
 rpm -ihv https://repo.zabbix.com/zabbix/5.2/rhel/7/x86_64/zabbix-agent-5.2.5-1.el7.x86_64.rpm
 
 #Generate a key
-openssl rand -hex 32 > /etc/zabbix/zabbix_agent.psk
-chown zabbix:zabbix /etc/zabbix/zabbix_agent.psk
-chmod 400 /etc/zabbix/zabbix_agent.psk
+openssl rand -hex 32 > $KEY
+chown zabbix:zabbix $KEY
+chmod 400 $KEY
 
 #Set up a configuration file
 cp $CONF $CONF.old
@@ -43,7 +44,7 @@ systemctl restart zabbix-agent
 echo ######################################################
 echo -e "\033[7mHostname=$(hostname)\033[0m"
 echo -e "\033[7mTLSPSKIdentity=PSK-$(hostname -s)\033[0m"
-echo -e "\033[7mPSK=$(cat /etc/zabbix/zabbix_agent.psk)\033[0m"
+echo -e "\033[7mPSK=$(cat $KEY)\033[0m"
 echo ######################################################
 else
 echo -en "\033[37;1;41mRun script with option: "./zabbix-agent-install.sh zabbix-server.example.com"\033[0m"
