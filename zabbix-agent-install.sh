5edb0a8e5399a812cbdc40580a5bf15bfa1a932a
Dmitriy Shweew <shweew@it-advisor.ru>
2021-03-12 13:51:40 +0400

    Update zabbix-agent-install.sh

--- zabbix-agent-install.sh
+++ zabbix-agent-install.sh
@@ -14,6 +14,9 @@ if [ -f "$FILE" ]; then
   wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+bionic_all.deb
   dpkg -i zabbix-release_5.0-1+bionic_all.deb
   sudo apt install zabbix-agent netfilter-persistent
+#  sudo ufw enable
+#  sudo ufw allow 10050/tcp
+# OR
   sudo iptables -A INPUT -p tcp --dport 10050 -j ACCEPT
   sudo netfilter-persistent save
   sudo netfilter-persistent reload
